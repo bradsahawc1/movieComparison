6c19b00568a9d83293009901270e79291f6c1acc
cbradshaw <cnbradshaw24@gmail.com>
2024-08-27 18:52:47 -0400

    Set up API so that I can make the autofill on the search bar

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+// data requests
+// http://www.omdbapi.com/?apikey=[513156eb]&
+// poster requests
+// http://img.omdbapi.com/?apikey=[513156eb]&
+// api key
+// 513156eb
+
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apiKey: '513156eb',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+};
+
+fetchData();
